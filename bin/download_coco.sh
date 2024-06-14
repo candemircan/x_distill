bb501d610b295df845d925eb64281ce3e5c33317
Can Demircan <cdemircan@tutanota.com>
2024-06-14 13:37:32 +0200

    reorganise download coco

--- bin/download_coco.sh
+++ bin/download_coco.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-cd $X_DISTILL/data
-mkdir coco
-cd coco
-mkdir images
-cd images
+# Set up directories
+DATA_DIR="$X_DISTILL/data/coco"
+IMAGES_DIR="$DATA_DIR/images"
+
+# Create directories if they don't exist
+mkdir -p "$IMAGES_DIR"
+
+# Download and extract image datasets
+cd "$IMAGES_DIR"
 wget -c http://images.cocodataset.org/zips/train2017.zip
 wget -c http://images.cocodataset.org/zips/val2017.zip
 wget -c http://images.cocodataset.org/zips/test2017.zip
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
+unzip -q train2017.zip
+unzip -q val2017.zip
+unzip -q test2017.zip
 
+# Clean up downloaded zip files
 rm train2017.zip
 rm val2017.zip
 rm test2017.zip
 
-cd ..
+# Download and extract annotations
+cd "$DATA_DIR"
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 
-unzip annotations_trainval2017.zip
+unzip -q annotations_trainval2017.zip
 
+# Clean up downloaded annotation zip file
 rm annotations_trainval2017.zip
